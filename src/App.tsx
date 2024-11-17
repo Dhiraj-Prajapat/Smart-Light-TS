df7d1eee28b84e300d50b3509a9567080bf798b6
Dhiraj Prajapat <106270089+Dhiraj-Prajapat@users.noreply.github.com>
2024-11-17 17:26:14 +0530

    project uploaded

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Ecosystem from "./pages/Ecosystem";
+import FeatureList from "./pages/FeatureList";
+import Footer from "./pages/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const App: React.FC = () => {
+  return (
+    <>
+      
+      <div>
+        <Home />
+        <Ecosystem />
+        <Products />
+        <FeatureList />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+
+export default App;
+
